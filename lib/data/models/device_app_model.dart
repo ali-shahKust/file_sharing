70f6f430be3450235a010297f11f7064588085a6
waseem_abbasi-at-828518973552 <waseem_abbasi-at-828518973552>
2022-04-22 11:08:02 +0500

    device app model added

---
+++ lib/data/models/device_app_model.dart
@@ -0,0 +1,8 @@
+import 'package:device_apps/device_apps.dart';
+
+class DeviceAppModel{
+  final Application apps;
+  bool isSelected;
+  DeviceAppModel({required this.apps,required this.isSelected});
+
+}
